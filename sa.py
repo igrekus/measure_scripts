d7436d5c6520eee9dbbe9896cde48d643b559822
igrekus <upload234@mail.ru>
2020-09-26 17:48:01 +0300

    MISC: clean up, remove trailing spaces

--- sa.py
+++ sa.py
@@ -10,7 +10,7 @@ src = rm.open_resource('USB0::0x0AAD::0x0135::039964506::0::INSTR')
 """
 source - set params - APPLY 3.3v,50ma,1
 source - measure current - MEAS:CURR?
-source - turn on output - OUTP:CHAN1 ON 
+source - turn on output - OUTP:CHAN1 ON
                           OUTP:MAST ON
 
 gen - turn on output - OUTP ON
@@ -19,7 +19,7 @@ gen - set power - :POW:POW 0.1dbm
 
 sa - set start freq - frequency:start 20mhz
 sa - set stop freq - frequency:stop 20ghz
-sa - turn on marker - calc1:mark1 on 
+sa - turn on marker - calc1:mark1 on
 sa - turn on marker - calc1:mark1:x 200mhz
 sa - display reference - level DISP:TRAC1:Y:RLEV -5dbm
 sa - switch to phase noise - INST:SEL PNOISE
