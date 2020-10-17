44fb8a54d56d4c58fbd5900ebabf1144386fe208
igrekus <upload234@mail.ru>
2020-10-17 12:33:14 +0300

    FEAT: add script for divider pow response measurementn

---
+++ divisor_1.py
@@ -0,0 +1,77 @@
+import datetime
+import time
+import visa
+
+import pandas as pd
+import numpy as np
+
+from config import instruments
+
+# --- параметры измерения ---
+# генератор
+f_start = 0.45   # начальная частота диапазона (ГГц)
+f_end = 15.0   # конечная частота диапазона (ГГЦ)
+f_step = 0.1   # шаг перестройки частоты (ГГц)
+p_in = 0   # мощность дБм
+
+# исочник питания
+u_source = 5.0   # напряжение питания (В)
+i_source = 100   # макс ток потребления (мА)
+
+# коэфициент деления
+coeff = 2
+# ---------------------------
+
+rm = visa.ResourceManager()
+
+gen = rm.open_resource(instruments['генератор 1'])
+sa = rm.open_resource(instruments['анализатор спектра'])
+src = rm.open_resource(instruments['источник питания 1'])
+
+file_name = f'xlsx/divisor-1-coeff_{coeff}-{datetime.datetime.now().isoformat().replace(":", ".")}.xlsx'
+
+
+def measure_1():
+    src.write(f'APPLY {u_source}V,{i_source}ma,1')
+    src.write('OUTP:CHAN1 ON')
+    src.write('OUTP:MAST ON')
+
+    # sa.write(f'DISP:TRAC1:Y:RLEV -5dbm')
+    sa.write(f'BAMD 200khz')
+    sa.write('frequency:start 1mhz')
+    sa.write('frequency:stop 30ghz')
+
+    result = []
+
+    fs = [round(x, 1) for x in np.linspace(f_start, f_end, int((f_end - f_start) / f_step) + 1, endpoint=True)]
+
+    for f_gen in fs:
+        print('set freq', f_gen)
+
+        gen.write(f'SOUR:FREQ:CW {f_gen}ghz')
+        gen.write(f':POW:POW {p_in}dbm')
+        gen.write('OUTP ON')
+
+        time.sleep(0.5)
+
+        f_sa = f_gen / coeff
+
+        sa.write('CALC1:MARK1 ON')
+        sa.write(f'CALC1:MARK1:X {f_sa}ghz')
+
+        time.sleep(0.5)
+
+        pw1 = sa.query(f'CALC1:MARK1:Y?')
+
+        print('read power', pw1)
+
+        result.append([f_gen, float(pw1)])
+
+    df = pd.DataFrame(result, columns=['F, GHz', f'Pout@F/{coeff}, dB'])
+    print(df)
+
+    df.to_excel(file_name)
+
+
+if __name__ == '__main__':
+    measure_1()
