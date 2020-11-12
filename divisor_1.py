7948f5474fa3265658ab62a8a58c9c209c6e535a
igrekus <upload234@mail.ru>
2020-11-12 17:47:59 +0300

    FEAT: add pow-freq plot

--- divisor_1.py
+++ divisor_1.py
@@ -6,6 +6,8 @@ import visa
 import numpy as np
 import pandas as pd
 
+from openpyxl.chart import LineChart, Reference
+
 from config import instruments
 
 # --- параметры измерения ---
@@ -72,6 +74,22 @@ def measure_1():
     print(df)
 
     df.to_excel(file_name)
+
+    wb = openpyxl.open(file_name)
+    ws = wb.active
+
+    rows = len(df)
+    data = Reference(ws, range_string=f'{ws.title}!C1:C{rows + 1}')
+    xs = Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}')
+
+    chart = LineChart()
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(xs)
+
+    ws.add_chart(chart, f'E4')
+
+    wb.close()
+    wb.save(file_name)
 
 
 if __name__ == '__main__':
