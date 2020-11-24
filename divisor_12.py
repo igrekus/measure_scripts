fceda843509c6512e7843f3e439e05c001238cf7
igrekus <upload234@mail.ru>
2020-11-24 18:31:24 +0300

    FEAT: implement plots for divisor-12 measurement

--- divisor_12.py
+++ divisor_12.py
@@ -1,4 +1,5 @@
 import datetime
+import openpyxl
 import time
 import visa
 
@@ -6,6 +7,8 @@ import pandas as pd
 import numpy as np
 
 from itertools import groupby
+from openpyxl.chart import Reference, LineChart
+from string import ascii_uppercase
 
 from config import instruments
 
@@ -84,14 +87,31 @@ def measure_12():
     res = {el[0]: list(el[1]) for el in groupby(result, key=lambda el: el[1])}
     res = {k: [el[2] for el in v] for k, v in res.items()}
 
+    cols = ['Fin, GHz'] + [f'Pout@F/{coeff}&Uin={u}, dBm' for u in res]
     df = pd.DataFrame(
         [[f] + pows for f, *pows in zip(freqs, *res.values())],
-        columns=['Fin, GHz'] + [f'Pout@F/{coeff}&Uin={u}, dBm' for u in res],
+        columns=cols,
     )
 
     print(df)
 
     df.to_excel(file_name)
+
+    wb = openpyxl.open(file_name)
+    ws = wb.active
+
+    rows = len(df)
+    data = Reference(ws, range_string=f'{ws.title}!C1:{ascii_uppercase[len(cols)]}{rows + 1}')
+    xs = Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}')
+
+    chart = LineChart()
+    chart.add_data(data, titles_from_data=True)
+    chart.set_categories(xs)
+
+    ws.add_chart(chart, f'G4')
+
+    wb.save(file_name)
+    wb.close()
 
 
 if __name__ == '__main__':
