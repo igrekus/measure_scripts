638b44572efb4adec1edb584ff7d2a18c70eda08
igrekus <upload234@mail.ru>
2021-03-23 16:59:52 +0300

    FEAT: implement VCO plots

--- process_vco.py
+++ process_vco.py
@@ -1,5 +1,10 @@
+import datetime
 import os
+
+import openpyxl
 import pandas as pd
+
+from openpyxl.chart import Reference, LineChart
 
 
 def _fileter_xlsx(path):
@@ -82,7 +87,123 @@ def main(path):
 
     result = pd.concat([df_1_2_25, df_1_2_60, df_1_2_85, df_3_4, df_6_25, df_6_60, df_6_85], axis=1)
 
-    result.to_excel('out.xlsx', engine='openpyxl')
+    out_excel_name = f'vco-result{datetime.datetime.now().isoformat().replace(":", ".")}.xlsx'
+    result.to_excel(out_excel_name, engine='openpyxl')
+
+    # save plots
+    wb = openpyxl.open(out_excel_name)
+    ws = wb.active
+
+    rows = len(result)
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!C1:C{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!I1:I{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!O1:O{rows + 1}'),
+        ],
+        title='Диапазон рабочих частот',
+        loc='B10'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!U1:W{rows + 1}'),
+        ],
+        title='Диапазон рабочих частот в зависимости от напряжения питания',
+        loc='K10'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!H1:H{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!N1:N{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!T1:T{rows + 1}'),
+        ],
+        title='Крутизна регулировочной характеристики',
+        loc='T10'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!D1:D{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!J1:J{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!P1:P{rows + 1}'),
+        ],
+        title='Выходная мощность',
+        loc='B25'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!E1:E{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!K1:K{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!Q1:Q{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!F1:F{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!L1:L{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!R1:R{rows + 1}'),
+        ],
+        title='Мощность выходного сигнала паразитных гармоник (2й и 3й) в диапазоне температур)',
+        loc='K25'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!G1:G{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!M1:M{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!S1:S{rows + 1}'),
+        ],
+        title='Ток потребления',
+        loc='T25'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!AD1:AD{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!AL1:AL{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!AT1:AT{rows + 1}'),
+        ],
+        title='Относительный уровень 2й гармоники',
+        loc='B40'
+    )
+
+    _add_chart(
+        ws=ws,
+        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+        ys=[
+            Reference(ws, range_string=f'{ws.title}!AE1:AE{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!AM1:AM{rows + 1}'),
+            Reference(ws, range_string=f'{ws.title}!AU1:AU{rows + 1}'),
+        ],
+        title='Относительный уровень 3й гармоники',
+        loc='K40'
+    )
+
+    wb.save(out_excel_name)
+    wb.close()
+
+
+def _add_chart(ws, xs, ys, title, loc):
+    chart = LineChart()
+    for y in ys:
+        chart.add_data(y, titles_from_data=True)
+    chart.set_categories(xs)
+    chart.title = title
+    ws.add_chart(chart, loc)
 
 
 if __name__ == '__main__':
