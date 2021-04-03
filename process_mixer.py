57cfafb4ef79dc7a71b66208a80cf36be94e1132
igrekus <upload234@mail.ru>
2021-04-03 13:26:51 +0300

    MISC: mixer measurement processing stub

--- process_mixer.py
+++ process_mixer.py
@@ -1,6 +1,8 @@
 import datetime
 import io
 import os
+from collections import defaultdict
+from pprint import pprint
 
 import openpyxl
 import pandas as pd
@@ -13,20 +15,87 @@ from openpyxl.chart import Reference, LineChart
 # todo 67-end -- 1-33, only lf1, lsb
 # todo 1-33, 34-66  --
 
-def _fileter_xlsx(path):
+csvs = [
+    '1-10.csv',
+    '1-100.csv',
+    '1-1500.csv',
+    '10.csv',
+    '11-10.csv',
+    '11-100.csv',
+    '11-1500.csv',
+    '12-12.csv',
+    '12-14.csv',
+    '12-16.csv',
+    '12-18.csv',
+    '13.csv',
+    '14.csv',
+    '15.csv',
+    '2-12.csv',
+    '2-14.csv',
+    '2-16.csv',
+    '2-18.csv',
+    '26-12.csv',
+    '26-14.csv',
+    '26-16.csv',
+    '26-18.csv',
+    '27.csv',
+    '28.csv',
+    '29.csv',
+    '3.csv',
+    '30.csv',
+    '31.csv',
+    '32.csv',
+    '33.csv',
+    '4.csv',
+    '5.csv',
+    '6-5.5.csv',
+    '6-6.5.csv',
+    '6-7.5.csv',
+    '7-12.csv',
+    '7-14.csv',
+    '7-16.csv',
+    '7-18.csv',
+    '8.csv',
+    '9.csv',
+]
+
+
+def _build_file_list(path):
     print('finding .csv')
-    return sorted([os.path.normpath(f'{path}/{p}') for p in os.listdir(path) if p.endswith('.csv')])
+    root = path
+
+    return {lf: {temp: {band: [
+        os.path.normpath(f'{root}/{lf} {temp}/{band}/{c}') for c in csvs
+    ] for band in ['lsb', 'usb']} for temp in ['+25', '+85', '-60']} for lf in ['LF1', 'LF2']}
 
 
-def _is_file_list_valid(files):
+def _validate_file_list(file_dict):
     print('validating file list')
     res = []
-    for file in files:
-        if any(ref in file for ref in ['1-10.csv', '1-100.csv', '1-1500.csv']):
-            res.append(file)
-        if any(ref in file for ref in ['3.csv', '4.csv', '5.csv']):
-            res.append(file)
-    return files == res and len(files) == 6
+    for lf, temps in file_dict.items():
+        for temp, bands in temps.items():
+            for band, files in bands.items():
+                for file in files:
+                    if not os.path.isfile(file):
+                        raise ValueError(f'нет .csv для LF={lf}, темп={temp}, полоса={band}: не хватает файла: {file}')
+
+
+def _read_csvs_to_dfs(file_dict):
+    out = {}
+    for lf, temps in file_dict.items():
+        tps = {}
+        for temp, bands in temps.items():
+            bds = {}
+            for band, files in bands.items():
+                dfs = []
+                for idx, file in enumerate(files):
+                    df = _read_csv(file)
+                    dfs.append(df)
+                    # dfs.append([file.split('\\')[-1], idx])
+                bds[band] = dfs
+            tps[temp] = bds
+        out[lf] = tps
+    return out
 
 
 def _read_csv(file):
@@ -34,90 +103,190 @@ def _read_csv(file):
         return pd.read_csv(io.StringIO('\n'.join(f.readlines()[2:])), sep=';').drop(['Unnamed: 2'], axis=1)
 
 
+def _build_1_cat_df(dfs):
+    filtered = [
+        dfs['LF1']['+25']['usb'][0],   # 1
+        dfs['LF1']['+25']['usb'][1],
+        dfs['LF1']['+25']['usb'][2],
+
+        dfs['LF1']['+25']['usb'][14],   # 2
+        dfs['LF1']['+25']['usb'][15],
+        dfs['LF1']['+25']['usb'][16],
+        dfs['LF1']['+25']['usb'][17],
+
+        dfs['LF1']['+25']['usb'][25],   # 3
+        dfs['LF1']['-60']['usb'][25],
+        dfs['LF1']['+85']['usb'][25],
+
+        dfs['LF1']['+25']['usb'][30],   # 4
+        dfs['LF1']['-60']['usb'][30],
+        dfs['LF1']['+85']['usb'][30],
+
+        dfs['LF1']['+25']['usb'][31],   # 5
+        dfs['LF1']['-60']['usb'][31],
+        dfs['LF1']['+85']['usb'][31],
+
+        dfs['LF1']['+25']['usb'][4],   # 11
+        dfs['LF1']['+25']['usb'][5],
+        dfs['LF1']['+25']['usb'][6],
+
+        dfs['LF1']['+25']['usb'][7],   # 12
+        dfs['LF1']['+25']['usb'][8],
+        dfs['LF1']['+25']['usb'][9],
+        dfs['LF1']['+25']['usb'][10],
+
+        dfs['LF1']['+25']['usb'][11],   # 13
+        dfs['LF1']['-60']['usb'][11],
+        dfs['LF1']['+85']['usb'][11],
+
+        dfs['LF1']['+25']['usb'][12],   # 14
+        dfs['LF1']['-60']['usb'][12],
+        dfs['LF1']['+85']['usb'][12],
+
+        dfs['LF1']['+25']['usb'][13],   # 15
+        dfs['LF1']['-60']['usb'][13],
+        dfs['LF1']['+85']['usb'][13],
+
+        dfs['LF1']['+25']['usb'][26],   # 30
+        dfs['LF1']['-60']['usb'][26],
+        dfs['LF1']['+85']['usb'][26],
+
+        dfs['LF1']['+25']['usb'][27],   # 31
+        dfs['LF1']['-60']['usb'][27],
+        dfs['LF1']['+85']['usb'][27],
+
+        dfs['LF1']['+25']['usb'][28],   # 32
+        dfs['LF1']['-60']['usb'][28],
+        dfs['LF1']['+85']['usb'][28],
+
+        dfs['LF1']['+25']['usb'][29],   # 33
+        dfs['LF1']['-60']['usb'][29],
+        dfs['LF1']['+85']['usb'][29],
+    ]
+
+    for idx in range(len(filtered)):
+        filtered[idx] = filtered[idx].applymap(lambda cell: float(cell.replace(',', '.')))
+        filtered[idx]['freq[Hz]'] = filtered[idx]['freq[Hz]'].apply(lambda row: row / 1_000_000_000)
+        filtered[idx].columns = ['freq[GHz]', filtered[idx].columns[-1]]
+
+    return pd.concat(objs=filtered, axis=1)
+
+
+def _build_2_cat_df(dfs):
+    # TODO implemet filter for cat2:
+    # 2 cat - 6-10, 26-29
+    filtered = [
+        dfs['LF1']['+25']['usb'][0],   # 1
+        dfs['LF1']['+25']['usb'][1],
+        dfs['LF1']['+25']['usb'][2],
+
+        dfs['LF1']['+25']['usb'][14],   # 2
+        dfs['LF1']['+25']['usb'][15],
+        dfs['LF1']['+25']['usb'][16],
+        dfs['LF1']['+25']['usb'][17],
+
+        dfs['LF1']['+25']['usb'][25],   # 3
+        dfs['LF1']['-60']['usb'][25],
+        dfs['LF1']['+85']['usb'][25],
+
+        dfs['LF1']['+25']['usb'][30],   # 4
+        dfs['LF1']['-60']['usb'][30],
+        dfs['LF1']['+85']['usb'][30],
+
+        dfs['LF1']['+25']['usb'][31],   # 5
+        dfs['LF1']['-60']['usb'][31],
+        dfs['LF1']['+85']['usb'][31],
+
+        dfs['LF1']['+25']['usb'][4],   # 11
+        dfs['LF1']['+25']['usb'][5],
+        dfs['LF1']['+25']['usb'][6],
+
+        dfs['LF1']['+25']['usb'][7],   # 12
+        dfs['LF1']['+25']['usb'][8],
+        dfs['LF1']['+25']['usb'][9],
+        dfs['LF1']['+25']['usb'][10],
+
+        dfs['LF1']['+25']['usb'][11],   # 13
+        dfs['LF1']['-60']['usb'][11],
+        dfs['LF1']['+85']['usb'][11],
+
+        dfs['LF1']['+25']['usb'][12],   # 14
+        dfs['LF1']['-60']['usb'][12],
+        dfs['LF1']['+85']['usb'][12],
+
+        dfs['LF1']['+25']['usb'][13],   # 15
+        dfs['LF1']['-60']['usb'][13],
+        dfs['LF1']['+85']['usb'][13],
+
+        dfs['LF1']['+25']['usb'][26],   # 30
+        dfs['LF1']['-60']['usb'][26],
+        dfs['LF1']['+85']['usb'][26],
+
+        dfs['LF1']['+25']['usb'][27],   # 31
+        dfs['LF1']['-60']['usb'][27],
+        dfs['LF1']['+85']['usb'][27],
+
+        dfs['LF1']['+25']['usb'][28],   # 32
+        dfs['LF1']['-60']['usb'][28],
+        dfs['LF1']['+85']['usb'][28],
+
+        dfs['LF1']['+25']['usb'][29],   # 33
+        dfs['LF1']['-60']['usb'][29],
+        dfs['LF1']['+85']['usb'][29],
+    ]
+
+    for idx in range(len(filtered)):
+        filtered[idx] = filtered[idx].applymap(lambda cell: float(cell.replace(',', '.')))
+        filtered[idx]['freq[Hz]'] = filtered[idx]['freq[Hz]'].apply(lambda row: row / 1_000_000_000)
+        filtered[idx].columns = ['freq[GHz]', filtered[idx].columns[-1]]
+
+    return pd.concat(objs=filtered, axis=1)
+
+
 def main(path):
-    files = _fileter_xlsx(path)
-    if not _is_file_list_valid(files):
-        raise ValueError('Неполный список файлов, в папке должны быть:\n'
-                         '- 3 файла с измерниями для пунктов 1,2,7,8,9,10,11,12 для всех температур\n'
-                         '- 1 файл с измерениям для пуктов 3,4\n'
-                         '- 3 файла с измерениями для пункта 6 для всех температур')
+    files = _build_file_list(path)
+    _validate_file_list(files)
+    dfs = _read_csvs_to_dfs(files)
+    # _build_out_dfs(dfs)
 
-    for file in files:
-        if any(ref in file for ref in ['1-10.csv', '1-100.csv', '1-1500.csv']):
-            if '1-10.csv' in file:
-                df_1_10 = _read_csv(file)
-            if '1-100.csv' in file:
-                df_1_100 = _read_csv(file)
-            if '1-1500.csv' in file:
-                df_1_1500 = _read_csv(file)
-        if any(ref in file for ref in ['3.csv', '4.csv', '5.csv']):
-            if '3.csv' in file:
-                df_3 = _read_csv(file)
-            if '4.csv' in file:
-                df_4 = _read_csv(file)
-            if '5.csv' in file:
-                df_5 = _read_csv(file)
+    # 1 cat - 1-5, 11-15, 30-33
+    # 2 cat - 6-10, 26-29
 
-    print('building data array')
-    df_1_10.columns = [f'{c}-10' if c != 'freq[Hz]' else 'freq[GHz]' for c in df_1_10.columns]
+    df_for_1_cat = _build_1_cat_df(dfs)
+    df_for_1_cat.to_excel(f'mixer-result-1-5_11-15_30-33-{datetime.datetime.now().isoformat().replace(":", ".")}.xlsx')
 
-    df_1_100 = df_1_100.drop(['freq[Hz]'], axis=1)
-    df_1_100.columns = [f'{c}-100' for c in df_1_100.columns]
-
-    df_1_1500 = df_1_1500.drop(['freq[Hz]'], axis=1)
-    df_1_1500.columns = [f'{c}-1500' for c in df_1_1500.columns]
-
-    df_3 = df_3.drop(['freq[Hz]'], axis=1)
-    df_3.columns = [f'{c}+25' for c in df_3.columns]
-
-    df_4 = df_4.drop(['freq[Hz]'], axis=1)
-    df_4.columns = [f'{c}-60' for c in df_4.columns]
-
-    df_5 = df_5.drop(['freq[Hz]'], axis=1)
-    df_5.columns = [f'{c}+85' for c in df_5.columns]
-
-    result = pd.concat([
-        df_1_10, df_1_100, df_1_1500,
-        df_3, df_4, df_5,
-    ], axis=1)
-
-    result = result.applymap(lambda x: float(x.replace(',', '.')))
-    result['freq[GHz]'] = result['freq[GHz]'].apply(lambda x: x / 1_000_000_000)
-    print(result)
-
-    out_excel_name = f'mixer-result{datetime.datetime.now().isoformat().replace(":", ".")}.xlsx'
-    result.to_excel(out_excel_name, engine='openpyxl')
+    print(df_for_1_cat)
 
     # save plots
-    print('making charts')
-    wb = openpyxl.open(out_excel_name)
-    ws = wb.active
-
-    rows = len(result)
-
-    _add_chart(
-        ws=ws,
-        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
-        ys=[
-            Reference(ws, range_string=f'{ws.title}!C1:E{rows + 1}'),
-        ],
-        title='Зависимость коэффициента преобразования от частоты входного сигнала при разной частоте ПЧ',
-        loc='B10'
-    )
-
-    _add_chart(
-        ws=ws,
-        xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
-        ys=[
-            Reference(ws, range_string=f'{ws.title}!F1:H{rows + 1}'),
-        ],
-        title='Зависимость коэффициента преобразования от частоты входного сигнала в диапазоне температур',
-        loc='K10'
-    )
-
-    print(f'saving resulting {out_excel_name}')
-    wb.save(out_excel_name)
-    wb.close()
+    # print('making charts')
+    # wb = openpyxl.open(out_excel_name)
+    # ws = wb.active
+    #
+    # rows = len(result)
+    #
+    # _add_chart(
+    #     ws=ws,
+    #     xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+    #     ys=[
+    #         Reference(ws, range_string=f'{ws.title}!C1:E{rows + 1}'),
+    #     ],
+    #     title='Зависимость коэффициента преобразования от частоты входного сигнала при разной частоте ПЧ',
+    #     loc='B10'
+    # )
+    #
+    # _add_chart(
+    #     ws=ws,
+    #     xs=Reference(ws, range_string=f'{ws.title}!B1:B{rows + 1}'),
+    #     ys=[
+    #         Reference(ws, range_string=f'{ws.title}!F1:H{rows + 1}'),
+    #     ],
+    #     title='Зависимость коэффициента преобразования от частоты входного сигнала в диапазоне температур',
+    #     loc='K10'
+    # )
+    #
+    # print(f'saving resulting {out_excel_name}')
+    # wb.save(out_excel_name)
+    # wb.close()
 
 
 def _add_chart(ws, xs, ys, title, loc):
@@ -131,5 +300,5 @@ def _add_chart(ws, xs, ys, title, loc):
 
 if __name__ == '__main__':
     # path = sys.argv
-    path = os.path.normpath('ref/s2p_process/mixer/lsb')
+    path = os.path.normpath('ref/s2p_process/mixer/measured_data/')
     main(path)
