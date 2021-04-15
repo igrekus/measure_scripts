4ffec8e91b5f0c53faa52b83fc70c68cd1c95b54
igrekus <upload234@mail.ru>
2021-04-15 18:32:22 +0300

    WIP: implement VCO data aggregator

--- process_vco.py
+++ process_vco.py
@@ -1,15 +1,49 @@
 import datetime
 import os
 
+from collections import defaultdict
+
 import openpyxl
 import pandas as pd
 
 from openpyxl.chart import Reference, LineChart
 
 
-def _fileter_xlsx(path):
+temperatures = [
+    '-20',
+    '-40',
+    '-60',
+    '+25',
+    '+40',
+    '+60',
+    '+85',
+    '0',
+]
+
+
+def _find_files(path):
     print('finding .xlsx')
-    return sorted([os.path.normpath(f'{path}/{p}') for p in os.listdir(path) if p.endswith('.xlsx')])
+    return {
+        t: _filter_xlsx(os.path.normpath(os.path.join(path, t)))
+        for t in temperatures
+    }
+
+
+def _filter_xlsx(path):
+    all_files = [p for p in os.listdir(path) if p.endswith('.xlsx')]
+
+    if len(all_files) != 3:
+        raise ValueError(f'в папке {path} должно быть три .xlsx-файла')
+
+    f1, f2, f3 = all_files
+    if not f1.startswith('vco-1-3-4-5par-'):
+        raise ValueError(f'в папке найден файл с неправильным форматом названия {f1}')
+    if not f2.startswith('vco-2par-'):
+        raise ValueError(f'в папке найден файл с неправильным форматом названия {f2}')
+    if not f3.startswith('vco-6tok-'):
+        raise ValueError(f'в папке найден файл с неправильным форматом названия {f2}')
+
+    return sorted([os.path.normpath(f'{path}/{p}') for p in all_files])
 
 
 def _is_file_list_valid(files):
@@ -36,62 +70,40 @@ def _is_file_list_valid(files):
 
 
 def main(path):
-    files = _fileter_xlsx(path)
-    if not _is_file_list_valid(files):
-        raise ValueError('Неполный список файлов, в папке должны быть:\n'
-                         '- 3 файла с измерниями для пунктов 1,2,7,8,9,10,11,12 для всех температур\n'
-                         '- 1 файл с измерениям для пуктов 3,4\n'
-                         '- 3 файла с измерениями для пункта 6 для всех температур')
+    all_files = _find_files(path)
+    prefixes = {
+        0: '1par',
+        1: '2par',
+        2: '6par',
+    }
 
-    for file in files:
-        print(f'reading {file}')
-        if 'vco-1-2' in file and file.endswith('+25.xlsx'):
-            df_1_2_25 = pd.read_excel(file, engine='openpyxl')
-        if 'vco-1-2' in file and file.endswith('-60.xlsx'):
-            df_1_2_60 = pd.read_excel(file, engine='openpyxl')
-        if 'vco-1-2' in file and file.endswith('+85.xlsx'):
-            df_1_2_85 = pd.read_excel(file, engine='openpyxl')
-        if 'vco-3-4' in file:
-            df_3_4 = pd.read_excel(file, engine='openpyxl')
-        if 'vco-6' in file and file.endswith('+25.xlsx'):
-            df_6_25 = pd.read_excel(file, engine='openpyxl')
-        if 'vco-6' in file and file.endswith('-60.xlsx'):
-            df_6_60 = pd.read_excel(file, engine='openpyxl')
-        if 'vco-6' in file and file.endswith('+85.xlsx'):
-            df_6_85 = pd.read_excel(file, engine='openpyxl')
+    dfs = []
+
+    for temp, files in list(all_files.items()):
+        for idx, file in enumerate(files):
+            df = pd.read_excel(file, engine='openpyxl', index_col=None)
+            df = df.drop(['Unnamed: 0'], axis=1)
+            if idx == 1:
+                df = df.pivot(index='Uc, V', columns='Usrc, V', values='F, Mhz')
+                df.columns = [f'F@Uc={c} V' for c in df.columns]
+                df['Uc, V'] = df.index
+                cs = list(df.columns)
+                df = df[[cs[-1]] + cs[:-1]]
+                df.reset_index(drop=True, inplace=True)
+
+            pref = prefixes[idx]
+            df.columns = [df.columns[0]] + [f'({temp}:{pref}) {c}' for c in df.columns[1:]]
+
+            dfs.append(df)
+
+    for i in range(1, len(dfs)):
+        dfs[i] = dfs[i].drop(dfs[i].columns[:1], axis=1)
 
     print('building data array')
-    # Usrc = 4.7V
-    df_1_2_25.columns = [f'{c}+25' if 'Uc, V' not in c else c for c in df_1_2_25.columns]
-    df_1_2_25 = df_1_2_25.drop(['Unnamed: 0+25'], axis=1)
-
-    df_1_2_60.columns = [f'{c}-60' for c in df_1_2_60.columns]
-    df_1_2_60 = df_1_2_60.drop(['Unnamed: 0-60', 'Uc, V-60'], axis=1)
-
-    df_1_2_85.columns = [f'{c}+85' for c in df_1_2_85.columns]
-    df_1_2_85 = df_1_2_85.drop(['Unnamed: 0+85', 'Uc, V+85'], axis=1)
-
-    df_3_4 = df_3_4.drop(['Unnamed: 0', 'Uc, V'], axis=1)
-
-    # Usrc = 5V
-    df_6_25.columns = [f'{c}+25' for c in df_6_25.columns]
-    df_6_25 = df_6_25.drop(['Unnamed: 0+25', 'Uc, V+25'], axis=1)
-    df_6_25['dPx2, dBm+25'] = df_6_25.apply(lambda row: row['Px2, bDm+25'] - row['Px1, bDm+25'], axis=1)
-    df_6_25['dPx3, dBm+25'] = df_6_25.apply(lambda row: row['Px3, bDm+25'] - row['Px1, bDm+25'], axis=1)
-
-    df_6_60.columns = [f'{c}-60' for c in df_6_60.columns]
-    df_6_60 = df_6_60.drop(['Unnamed: 0-60', 'Uc, V-60'], axis=1)
-    df_6_60['dPx2, dBm-60'] = df_6_60.apply(lambda row: row['Px2, bDm-60'] - row['Px1, bDm-60'], axis=1)
-    df_6_60['dPx3, dBm-60'] = df_6_60.apply(lambda row: row['Px3, bDm-60'] - row['Px1, bDm-60'], axis=1)
-
-    df_6_85.columns = [f'{c}+85' for c in df_6_85.columns]
-    df_6_85 = df_6_85.drop(['Unnamed: 0+85', 'Uc, V+85'], axis=1)
-    df_6_85['dPx2, dBm+85'] = df_6_85.apply(lambda row: row['Px2, bDm+85'] - row['Px1, bDm+85'], axis=1)
-    df_6_85['dPx3, dBm+85'] = df_6_85.apply(lambda row: row['Px3, bDm+85'] - row['Px1, bDm+85'], axis=1)
-
-    result = pd.concat([df_1_2_25, df_1_2_60, df_1_2_85, df_3_4, df_6_25, df_6_60, df_6_85], axis=1)
 
     out_excel_name = f'vco-result{datetime.datetime.now().isoformat().replace(":", ".")}.xlsx'
+
+    result = pd.concat(objs=dfs, axis=1)
     result.to_excel(out_excel_name, engine='openpyxl')
 
     # save plots
@@ -214,5 +226,5 @@ def _add_chart(ws, xs, ys, title, loc):
 
 if __name__ == '__main__':
     # path = sys.argv
-    path = os.path.normpath('ref/s2p_process/vco/xlsx')
+    path = os.path.normpath(r'.\ref\s2p_process\vco\2021-04-14')
     main(path)
